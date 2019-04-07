dc6d95547ac0ded326c5358ede808e48ce7a0556
NikDoom <49077681+NikDoom@users.noreply.github.com>
2019-04-07 23:41:40 +0300

    game

---
+++ MyGame/src/js/name.js
@@ -0,0 +1,17 @@
+var nameInput = document.getElementById("nameInput");
+var nameForm = document.getElementById("nameForm");
+var playerName = document.getElementById("playerName");
+var nameWrap = document.getElementById("nameWrap");
+var wrap = document.getElementById("wrap");
+var lo = document.getElementById("lo");
+
+nameForm.onsubmit = function() {
+	let nameValue = nameInput.value;
+	event.preventDefault();
+	if (nameValue != "") {
+		playerName.textContent = nameValue;
+	}
+	nameWrap.style.display = "none";
+	wrap.style.display = "block";
+	return false
+}
--- MyGame/src/js/script.js
+++ MyGame/src/js/script.js
@@ -9,8 +9,12 @@ themes.sort(compareRandom);
 console.log(themes);
 
 let titles = document.getElementsByClassName('game-item_title');
+let row = document.getElementsByClassName('game-row');
+let cell = document.getElementsByClassName('game-item_cell');
 
 titles[0].textContent = themes[0];
 titles[1].textContent = themes[1];
 titles[2].textContent = themes[2];
 titles[3].textContent = themes[3];
+
+console.log(typeof(cell[0].textContent));
